dabad75768af05957915cb95fabcbf5c3807546f
ronttizz <toni.nissinen@gmail.com>
2022-03-30 09:49:38 +0300

    Added some CSS and figured margin and padding errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import classes from "./App.module.css";
 import AnimalCard from "./UI components/AnimalCard";
+// import animals from "./animals.js";
 
 function App() {
+  // state = {
+  //   animalsList: animals,
+  // };
   return (
     <div>
       <div className={classes.AnimalCardContainer}>
--- src/UI components/AnimalCard.js
+++ src/UI components/AnimalCard.js
@@ -1,13 +1,19 @@
 import classes from "./AnimalCard.module.css";
 import Button from "./Button";
 
-function AnimalCard() {
+function AnimalCard(props) {
   return (
     <div className={classes.animalCard}>
-      <img src="#" alt="#"></img>
-      <h2>AnimalName</h2>
+      <img
+        src="https://source.unsplash.com/1600x900/?cat"
+        alt="Cat"
+        className={classes.image}
+      ></img>
+      <h2 className={classes.animalName}>AnimalName</h2>
       <div className={classes.bottom}>
-        <p>❤️</p>
+        <p>
+          <span className={classes.likes}>0</span> ❤️
+        </p>
         <Button />
       </div>
     </div>
